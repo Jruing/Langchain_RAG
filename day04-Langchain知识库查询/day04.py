8e8a58c1e6e571b24ebc98477859d9754b6bf63e
Jruing <jruing@126.com>
2024-11-22 18:11:23 +0800

    langchain开发rag学习笔记

--- day04-Langchain知识库查询/day04.py
+++ day04-Langchain知识库查询/day04.py
@@ -2,21 +2,25 @@
 import os
 
 import lancedb
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain_community.chat_models import ChatOllama
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_community.vectorstores import LanceDB
-from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 
 embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
 db = lancedb.connect(os.path.join(os.path.dirname(os.getcwd()), "lancedb"))
 vectordb = LanceDB(connection=db, embedding=embeddings, table_name='langchain_test')
 
-
 user_question = "Cobra创建子命令"
 # 实例化LLM
 llm = ChatOllama(base_url="http://localhost:11434", model="qwen2.5:0.5b")
+retriever_from_llm = MultiQueryRetriever.from_llm(
+    retriever=vectordb.as_retriever(), llm=llm
+)
 
 # 声明系统提示模板,接受一个变量 <language>,目标语言
 system_template = """
@@ -28,10 +32,18 @@ system_template = """
 - 2. 若无结果直接回复未找到相关方案
 """
 question = "python是什么"
-sim_docs = vectordb.similarity_search(question, k=3)
-for i, sim_doc in enumerate(sim_docs):
-    print(f"检索到的第{i}个内容: \n{sim_doc.page_content}", end="\n--------------\n")
-# 声明用户提示模板，接受一个变量<text>，翻译内容
+# sim_docs = vectordb.similarity_search(question, k=3)
+sim_docs = vectordb.from_texts([question], embedding=embeddings)
+retriever = sim_docs.as_retriever()
+print(retriever)
+
+
+def format_docs(docs):
+    doc_strings = [doc.page_content for doc in docs]
+    return "\n\n".join(doc_strings)
+
+
+# # 声明用户提示模板，接受一个变量<text>，翻译内容
 user_template = "{question}"
 # 创建提示模板
 prompt_template = ChatPromptTemplate.from_messages(
@@ -39,9 +51,10 @@ prompt_template = ChatPromptTemplate.from_messages(
 )
 print("========提示词模板========")
 # 使用LCEL组装一个链，将模板、LLM和输出解析器三者连接起来
-chain = prompt_template | llm | StrOutputParser()
+chain = {"content": retriever_from_llm | format_docs,
+         "question": RunnablePassthrough()} | prompt_template | llm | StrOutputParser()
 print("========封装调用链========")
 # 调用链
-chain_rs = chain.invoke({"question": question, "content": "\n".join([i.page_content for i in sim_docs])})
+chain_rs = chain.invoke("langchain是什么")
 # 打印结果
 print("========调用链执行结果========\n", chain_rs)
